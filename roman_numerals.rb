dc3ed5acaf64cd835db79ec8d403ebb691ddf78d
theaamanda <ta.kupler@gmail.com>
2016-03-28 21:53:14 +0200

    new programs written

---
+++ roman_numerals.rb
@@ -0,0 +1,28 @@
+# Old-school Roman numerals.​ In the early days of Roman numerals, the Romans didn’t bother with any of this new-fangled subtraction “IX” nonsense. 
+# No sir, it was straight addition, biggest to littlest—so 9 was written “VIIII,” and so on. Write a method that when passed an integer 
+# between 1 and 3000 (or so) returns a string containing the proper old-school Roman numeral. In other words, ​old_roman_numeral 4​ should return 'IIII'. 
+# Make sure to test your method on a bunch of different numbers. ​Hint:​ Use the integer division and modulus methods here.
+
+# For reference, these are the values of the letters used:
+
+# I = 1	V = 5	X = 10	L = 50
+# C = 100	D = 500	M = 1000
+# ​“Modern” Roman numerals.​ Eventually, someone thought it would be terribly clever if putting a smaller number before a larger one meant you had to 
+# subtract the smaller one. As a result of this development, you must now suffer. 
+# Rewrite your previous method to return the new-style Roman numerals so when someone calls ​roman_numeral 4​, it should return 'IV'.
+
+def roman number
+	rl = "" #emtpy roman letter string
+	counter = 0
+	numb_array = [1000,500,100,50,10,5,1]
+	letter_array = ['M','D','C','L','X','V','I']
+	numb_array.each do |numb|
+		#numb=jeweilige zahl vom array
+		rl = rl+(letter_array[counter]*(number/numb))
+		number = number%numb
+		counter = counter + 1	
+	end
+	return rl
+end
+
+puts roman(1359)

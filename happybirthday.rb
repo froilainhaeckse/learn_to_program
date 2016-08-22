bf241014ae78fc90474511efb42b2ea4e38ec3c1
Thea Amanda <ta.kupler@gmail.com>
2016-08-22 14:58:36 +0200

    finished 12.2

--- happybirthday.rb
+++ happybirthday.rb
@@ -3,5 +3,20 @@ puts "Tell me the year in which you were born (YYYY):"
 year = gets.chomp
 puts "Tell me the month in which you were born (MM):"
 month = gets.chomp
+while month.to_i >= 13
+  puts "Something went wrong. Try again. Which month were you born?"
+  month = gets.chomp
+end
 puts "Tell me the day on which you were born (DD):"
 day = gets.chomp
+
+# age in seconds
+sec_age = Time.now - Time.gm(year, month, day)
+# age in hours
+hrs_age = sec_age / 3600
+age = hrs_age / 8736
+counter = age.to_i
+while counter >= 1
+  puts "SPANK! You had birthday!"
+  counter = counter - 1
+end

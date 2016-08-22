e84d84b9ce1cfaa6eecc4c35d3aadd2694d14d09
Thea Amanda <ta.kupler@gmail.com>
2016-08-22 14:24:28 +0200

    task 12.2

---
+++ happybirthday.rb
@@ -0,0 +1,7 @@
+# • Happy birthday! Ask what year a person was born in, then the month, and then the day. Figure out how old they are, and give them a big SPANK! for each birthday they have had.
+puts "Tell me the year in which you were born (YYYY):"
+year = gets.chomp
+puts "Tell me the month in which you were born (MM):"
+month = gets.chomp
+puts "Tell me the day on which you were born (DD):"
+day = gets.chomp
